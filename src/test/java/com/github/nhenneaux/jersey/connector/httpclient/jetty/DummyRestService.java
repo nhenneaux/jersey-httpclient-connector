31018d7540102d801e545dac3477f307125edfd1
Nicolas Henneaux <Nicolas.Henneaux@gmail.com>
2021-02-16 12:09:01 +0100

    Add Jetty tests

---
+++ src/test/java/com/github/nhenneaux/jersey/connector/httpclient/jetty/DummyRestService.java
@@ -0,0 +1,27 @@
+package com.github.nhenneaux.jersey.connector.httpclient.jetty;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.concurrent.TimeUnit;
+
+@Path("/")
+public class DummyRestService {
+
+    @HEAD
+    @Path("ping")
+    public void ping() {
+        // Just checking the server is listening
+    }
+
+    @GET
+    @Path("pingWithSleep")
+    public long pingWithSleep(@QueryParam("sleepTimeInMilliseconds") long sleepTimeInMilliseconds) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(sleepTimeInMilliseconds);
+        return sleepTimeInMilliseconds;
+    }
+
+
+}
