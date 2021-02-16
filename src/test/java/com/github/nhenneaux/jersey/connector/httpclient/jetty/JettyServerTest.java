bfe1f7fbaa30fac3d0cd6ea1b9605ab383f584cf
Nicolas Henneaux <Nicolas.Henneaux@gmail.com>
2021-02-16 12:09:01 +0100

    Add Jetty tests

---
+++ src/test/java/com/github/nhenneaux/jersey/connector/httpclient/jetty/JettyServerTest.java
@@ -0,0 +1,194 @@
+package com.github.nhenneaux.jersey.connector.httpclient.jetty;
+
+import com.github.nhenneaux.jersey.connector.httpclient.HttpClientConnector;
+import org.glassfish.jersey.client.ClientConfig;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpClient;
+import java.security.KeyStore;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.github.nhenneaux.jersey.connector.httpclient.jetty.JettyServer.TlsSecurityConfiguration.getKeyStore;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings("squid:S00112")
+class JettyServerTest {
+    static final int PORT = 2223;
+    private static final String PING = "/ping";
+
+    private static WebTarget getClient(int port, KeyStore trustStore, ClientConfig clientConfig) {
+        return ClientBuilder.newBuilder()
+                .trustStore(trustStore)
+                .withConfig(clientConfig)
+                .build()
+                .target("https://localhost:" + port);
+    }
+
+    private static ClientConfig http2ClientConfig() {
+        return new ClientConfig()
+                .connectorProvider(HttpClientConnector::new);
+    }
+
+    static AutoCloseable jerseyServer(int port, JettyServer.TlsSecurityConfiguration tlsSecurityConfiguration, final Class<?>... serviceClasses) {
+        return new JettyServer(port, tlsSecurityConfiguration, serviceClasses);
+    }
+
+    static WebTarget getClient(int port) {
+        return getClient(port, trustStore(), http2ClientConfig());
+    }
+
+    private static KeyStore trustStore() {
+        return getKeyStore("pkcs12-truststore-password".toCharArray(), "truststore.p12");
+    }
+
+    static JettyServer.TlsSecurityConfiguration tlsConfig() {
+        return new JettyServer.TlsSecurityConfiguration(
+                getKeyStore("TEST==ONLY==jks-keystore-password".toCharArray(), "keystore.p12"),
+                "server",
+                "TEST==ONLY==jks-keystore-password",
+                "TLSv1.2"
+        );
+    }
+
+    @Test
+    @Timeout(20)
+    void testValidTls() throws Exception {
+        int port = PORT;
+        JettyServer.TlsSecurityConfiguration tlsSecurityConfiguration = tlsConfig();
+        try (AutoCloseable ignored = jerseyServer(
+                port,
+                tlsSecurityConfiguration,
+                DummyRestService.class)) {
+            final Response ping = getClient(port).path(PING).request().head();
+            assertEquals(204, ping.getStatus());
+        }
+    }
+
+    @Test
+    @Timeout(60)
+    void testConcurrent() throws Exception {
+        testConcurrent(http2ClientConfig());
+    }
+
+    @Test
+    @Timeout(60)
+    void testConcurrentHttp1() throws Exception {
+        testConcurrent(new ClientConfig());
+    }
+
+    @Test
+    @Timeout(60)
+    void testConcurrentHttp2JavaHttpClient() throws Exception {
+        testConcurrent(new ClientConfig()
+                .connectorProvider((jaxRsClient, configuration) -> getHttpClientConnector(jaxRsClient, HttpClient.Version.HTTP_2)));
+    }
+
+    @Test
+    @Timeout(60)
+    void testConcurrentGetHttp2JavaHttpClient() throws Exception {
+        testConcurrent(new ClientConfig()
+                .connectorProvider((jaxRsClient, configuration) -> getHttpClientConnector(jaxRsClient, HttpClient.Version.HTTP_2)), HttpMethod.GET, "/pingWithSleep");
+    }
+
+    @Test
+    @Timeout(60)
+    void testConcurrentHttp1JavaHttpClient() throws Exception {
+        testConcurrent(new ClientConfig()
+                .connectorProvider((jaxRsClient, configuration) -> getHttpClientConnector(jaxRsClient, HttpClient.Version.HTTP_1_1)));
+    }
+
+    private HttpClientConnector getHttpClientConnector(Client jaxRsClient, HttpClient.Version version) {
+        return new HttpClientConnector(HttpClient.newBuilder().sslContext(jaxRsClient.getSslContext()).version(version).build());
+    }
+
+
+    private void testConcurrent(ClientConfig clientConfig) throws Exception {
+        testConcurrent(clientConfig, "HEAD", PING);
+    }
+
+    private void testConcurrent(ClientConfig clientConfig, String method, String path) throws Exception {
+        int port = PORT;
+        JettyServer.TlsSecurityConfiguration tlsSecurityConfiguration = tlsConfig();
+        final KeyStore truststore = trustStore();
+        try (AutoCloseable ignored = jerseyServer(
+                port,
+                tlsSecurityConfiguration,
+                DummyRestService.class)) {
+            final int nThreads = 4;
+            final int iterations = 10_000;
+            // Warmup
+            Client client = ClientBuilder.newBuilder()
+                    .trustStore(truststore)
+                    .withConfig(clientConfig)
+                    .build();
+            client.target("https://localhost:" + port).path(path).request().method(method).close();
+
+            AtomicInteger counter = new AtomicInteger();
+            final Runnable runnable = () -> {
+                long start = System.nanoTime();
+                for (int i = 0; i < iterations; i++) {
+                    try (Response response = client
+                            .target("https://localhost:" + port).path(path).request().method(method)) {
+                        response.readEntity(InputStream.class).readAllBytes();
+                        response.getStatus();
+                        counter.incrementAndGet();
+                        int reportEveryRequests = 1_000;
+                        if (i % reportEveryRequests == 0) {
+                            System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start) * 1.0 / reportEveryRequests);
+                            start = System.nanoTime();
+                        }
+                    } catch (IOException e) {
+                        throw new IllegalStateException(e);
+                    }
+                }
+            };
+            Thread.setDefaultUncaughtExceptionHandler((t1, e) -> e.printStackTrace());
+            final Set<Thread> threads = IntStream
+                    .range(0, nThreads)
+                    .mapToObj(i -> runnable)
+                    .map(Thread::new)
+                    .collect(Collectors.toSet());
+
+            threads.forEach(Thread::start);
+
+
+            for (Thread thread : threads) {
+                thread.join();
+            }
+
+            assertEquals((long) nThreads * iterations, counter.get());
+
+        }
+    }
+
+
+    @Test
+    void shouldWorkInLoop() throws Exception {
+        int port = PORT;
+        JettyServer.TlsSecurityConfiguration tlsSecurityConfiguration = tlsConfig();
+        for (int i = 0; i < 100; i++) {
+            try (
+                    @SuppressWarnings("unused") WeldContainer container = new Weld().initialize();
+                    AutoCloseable ignored = jerseyServer(port, tlsSecurityConfiguration, DummyRestService.class)
+            ) {
+                assertEquals(204, getClient(port).path(PING).request().head().getStatus());
+            }
+        }
+    }
+
+
+}
---
+++ test-master/com/github/nhenneaux/jersey/connector/httpclient/HttpClientConnectorTest.java
@@ -0,0 +1,270 @@
+package com.github.nhenneaux.jersey.connector.httpclient;
+
+import jakarta.ws.rs.ProcessingException;
+import jakarta.ws.rs.client.Client;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.ClientRequest;
+import org.glassfish.jersey.client.ClientResponse;
+import org.glassfish.jersey.client.spi.AsyncConnectorCallback;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.mockito.ArgumentCaptor;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.net.Proxy;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+class HttpClientConnectorTest {
+
+    static final Class<? extends HttpRequest.BodyPublisher> IS_PUBLISHER_CLASS = HttpRequest.BodyPublishers.ofInputStream(() -> null).getClass();
+    @SuppressWarnings("unchecked")
+    static final Class<? extends HttpResponse.BodyHandler<InputStream>> IS_HANDLER_CLASS = (Class<? extends HttpResponse.BodyHandler<InputStream>>) HttpResponse.BodyHandlers.ofInputStream().getClass();
+
+    @Test
+    void streamRequestBodySuccess() throws ExecutionException, InterruptedException {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        final HttpRequest httpRequest = mock(HttpRequest.class);
+        @SuppressWarnings("unchecked") final HttpResponse<InputStream> httpResponse = mock(HttpResponse.class);
+        final CompletableFuture<HttpResponse<InputStream>> responseFuture = CompletableFuture.completedFuture(httpResponse);
+        when(httpClient.sendAsync(eq(httpRequest), any(IS_HANDLER_CLASS))).thenReturn(responseFuture);
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+
+        final HttpRequest.Builder requestBuilder = mock(HttpRequest.Builder.class);
+        final HttpRequest.Builder requestBuilderWithMethod = mock(HttpRequest.Builder.class);
+        final ClientRequest clientRequest = mock(ClientRequest.class);
+        final String method = "POST";
+        when(clientRequest.getMethod()).thenReturn(method);
+        when(requestBuilder.method(eq(method), any(IS_PUBLISHER_CLASS))).thenReturn(requestBuilderWithMethod);
+        when(requestBuilderWithMethod.build()).thenReturn(httpRequest);
+        // When
+        final CompletableFuture<HttpResponse<InputStream>> httpResponseCompletableFuture = httpClientConnector.streamRequestBody(clientRequest, requestBuilder);
+
+        // Then
+        await()
+                .atMost(Duration.ofSeconds(5L))
+                .until(httpResponseCompletableFuture::isDone);
+        assertSame(httpResponse, httpResponseCompletableFuture.get());
+    }
+
+    @Test
+    void close() {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+        // When
+        httpClientConnector.close();
+
+        // Then
+        verifyNoInteractions(httpClient);
+    }
+
+    @Test
+    void streamRequestBodyFailure() throws IOException {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        final HttpRequest httpRequest = mock(HttpRequest.class);
+        final CompletableFuture<HttpResponse<InputStream>> responseFuture = CompletableFuture.completedFuture(null);
+        when(httpClient.sendAsync(eq(httpRequest), any(IS_HANDLER_CLASS))).thenReturn(responseFuture);
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+
+        final HttpRequest.Builder requestBuilder = mock(HttpRequest.Builder.class);
+        final HttpRequest.Builder requestBuilderWithMethod = mock(HttpRequest.Builder.class);
+        final ClientRequest clientRequest = mock(ClientRequest.class);
+        final String method = "POST";
+        when(clientRequest.getMethod()).thenReturn(method);
+        when(requestBuilder.method(eq(method), any(IS_PUBLISHER_CLASS))).thenReturn(requestBuilderWithMethod);
+        when(requestBuilderWithMethod.build()).thenReturn(httpRequest);
+        final IOException ioException = new IOException(UUID.randomUUID().toString());
+        doThrow(ioException).when(clientRequest).writeEntity();
+        // When
+        final CompletableFuture<HttpResponse<InputStream>> httpResponseCompletableFuture = httpClientConnector.streamRequestBody(clientRequest, requestBuilder);
+
+        // Then
+        await()
+                .atMost(Duration.ofSeconds(5L))
+                .until(httpResponseCompletableFuture::isDone);
+        assertTrue(httpResponseCompletableFuture.isCompletedExceptionally());
+        final ExecutionException executionException = assertThrows(ExecutionException.class, httpResponseCompletableFuture::get);
+        assertEquals("The sending process failed with I/O error, " + ioException.getMessage(), executionException.getCause().getMessage());
+        assertSame(ioException, executionException.getCause().getCause());
+    }
+
+    @Test
+    void shouldIgnoreObjectsInIsGreaterThanZero() {
+        assertFalse(HttpClientConnector.isGreaterThanZero(new Object()));
+    }
+
+    @Test
+    void shouldHandleNegativeInIsGreaterThanZero() {
+        assertFalse(HttpClientConnector.isGreaterThanZero(-1));
+    }
+
+    @Test
+    void shouldHandleZeroInIsGreaterThanZero() {
+        assertFalse(HttpClientConnector.isGreaterThanZero(0));
+    }
+
+    @Test
+    void shouldHandlePositiveInIsGreaterThanZero() {
+        assertTrue(HttpClientConnector.isGreaterThanZero(1));
+    }
+
+    @Test
+    void shouldThrowWhenConnectingStreamAlreadyConnected() throws IOException {
+        final PipedInputStream pipedInputStream = new PipedInputStream();
+        final PipedOutputStream pipedOutputStream = new PipedOutputStream();
+        pipedOutputStream.connect(pipedInputStream);
+        final ProcessingException expectedException = assertThrows(ProcessingException.class, () -> HttpClientConnector.connectStream(pipedOutputStream, pipedInputStream));
+        assertEquals("The input stream cannot be connected to the output stream, Already connected", expectedException.getMessage());
+    }
+
+    @Test
+    void shouldHandleInterruption() {
+        final InterruptedException interruptedException = new InterruptedException(UUID.randomUUID().toString());
+        final Thread thread = new Thread(() -> HttpClientConnector.handleInterruption(() -> {
+            throw interruptedException;
+        }));
+        AtomicReference<Throwable> expectedInterruptedException = new AtomicReference<>();
+        thread.setUncaughtExceptionHandler((t, e) -> expectedInterruptedException.set(e));
+        thread.start();
+
+        await()
+                .atMost(Duration.ofSeconds(2L))
+                .until(() -> !thread.isAlive());
+        assertFalse(thread::isAlive);
+        assertEquals(ProcessingException.class, expectedInterruptedException.get().getClass());
+        assertSame(interruptedException, expectedInterruptedException.get().getCause());
+
+    }
+
+    @Test
+    @Timeout(1)
+    void shouldHandleExceptionWhenWaitingResponse() {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        final CompletableFuture<HttpResponse<InputStream>> responseFuture = new CompletableFuture<>();
+
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+
+        final String message = UUID.randomUUID().toString();
+        final Exception expectedException = new Exception(message);
+        responseFuture.completeExceptionally(expectedException);
+
+        // When
+        final ProcessingException processingException = assertThrows(ProcessingException.class, () -> httpClientConnector.waitResponse(responseFuture, 10));
+
+        // Then
+        assertEquals("The async sending process failed with error, java.lang.Exception: " + message, processingException.getMessage());
+        assertSame(expectedException, processingException.getCause().getCause());
+    }
+
+    @Test
+    void shouldUseTimeoutWhenNonZeroReadTimeout() throws InterruptedException, ExecutionException, TimeoutException {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        @SuppressWarnings("unchecked") final CompletableFuture<HttpResponse<InputStream>> responseFuture = mock(CompletableFuture.class);
+
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+
+
+        // When
+        httpClientConnector.waitResponse(responseFuture, 10);
+
+        // Then
+        verify(responseFuture).get(10, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    void shouldNotUseTimeoutGetWhenZeroReadTimeout() throws InterruptedException, ExecutionException {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        @SuppressWarnings("unchecked") final CompletableFuture<HttpResponse<InputStream>> responseFuture = mock(CompletableFuture.class);
+
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+
+
+        // When
+        httpClientConnector.waitResponse(responseFuture, 0);
+
+        // Then
+        verify(responseFuture).get();
+    }
+
+    @Test
+    void shouldHandleCallbackOnFailure() {
+        // Given
+        final HttpClient httpClient = mock(HttpClient.class);
+        final CompletableFuture<HttpResponse<InputStream>> responseFuture = new CompletableFuture<>();
+        final String message = UUID.randomUUID().toString();
+        final Exception expectedException = new Exception(message);
+        responseFuture.completeExceptionally(expectedException);
+
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(httpClient);
+
+        final AsyncConnectorCallback asyncConnectorCallback = mock(AsyncConnectorCallback.class);
+        // When
+        final Future<ClientResponse> clientResponseFuture = httpClientConnector.toJerseyResponseWithCallback(null, responseFuture, asyncConnectorCallback);
+
+        // Then
+        assertTrue(clientResponseFuture.isDone());
+
+        final ExecutionException executionException = assertThrows(ExecutionException.class, clientResponseFuture::get);
+        assertSame(expectedException, executionException.getCause());
+        final ArgumentCaptor<CompletionException> completionExceptionArgumentCaptor = ArgumentCaptor.forClass(CompletionException.class);
+        verify(asyncConnectorCallback).failure(completionExceptionArgumentCaptor.capture());
+        assertSame(expectedException, completionExceptionArgumentCaptor.getValue().getCause());
+    }
+
+    @Test
+    void shouldConfigureProxy() throws NoSuchAlgorithmException {
+        // Given
+        final ClientConfig configuration = new ClientConfig().property(ClientProperties.PROXY_URI, "https://my.gateway.io:3129");
+        final Client client = mock(Client.class);
+        when(client.getSslContext()).thenReturn(SSLContext.getDefault());
+        // When
+        final HttpClientConnector httpClientConnector = new HttpClientConnector(client, configuration);
+        // Then
+        assertTrue(httpClientConnector.getHttpClient().proxy().isPresent());
+        final List<Proxy> proxies = httpClientConnector.getHttpClient().proxy().orElseThrow().select(URI.create("https://my.service.io"));
+        assertEquals(1, proxies.size());
+        final Proxy proxy = proxies.get(0);
+        assertThat(proxy.address().toString(), anyOf(equalTo("my.gateway.io:3129"), equalTo("my.gateway.io/<unresolved>:3129")));
+    }
+}
