52a932bd8e398dfe8fe73da8b5c78b14210eb862
Nicolas Henneaux <Nicolas.Henneaux@gmail.com>
2021-02-16 12:27:31 +0100

    Avoid closing stream for the request since the entity is not sent in this case

--- src/main/java/com/github/nhenneaux/jersey/connector/httpclient/HttpClientConnector.java
+++ src/main/java/com/github/nhenneaux/jersey/connector/httpclient/HttpClientConnector.java
@@ -162,17 +162,20 @@ public class HttpClientConnector implements Connector {
     }
 
     CompletableFuture<HttpResponse<InputStream>> streamRequestBody(ClientRequest clientRequest, HttpRequest.Builder requestBuilder) {
+        @SuppressWarnings("squid:S2095") // The stream cannot be closed here and is closed in Jersey client.
         final PipedOutputStream pipedOutputStream = new PipedOutputStream();
+        @SuppressWarnings("squid:S2095") // The stream cannot be closed here and is closed in Jersey client.
         final PipedInputStream pipedInputStream = new PipedInputStream();
         connectStream(pipedOutputStream, pipedInputStream);
         clientRequest.setStreamProvider(contentLength -> pipedOutputStream);
+
         final HttpRequest httpRequest = requestBuilder
                 .method(clientRequest.getMethod(), HttpRequest.BodyPublishers.ofInputStream(() -> pipedInputStream))
                 .build();
         final CompletableFuture<HttpResponse<InputStream>> httpResponseCompletableFuture = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
 
         final Runnable entityWriter = () -> {
-            try (pipedInputStream; pipedOutputStream) {
+            try {
                 clientRequest.writeEntity();
             } catch (IOException e) {
                 throw new ProcessingException("The sending process failed with I/O error, " + e.getMessage(), e);
--- src/test/java/com/github/nhenneaux/jersey/connector/httpclient/DummyRestService.java
+++ src/test/java/com/github/nhenneaux/jersey/connector/httpclient/DummyRestService.java
@@ -1,9 +1,13 @@
 package com.github.nhenneaux.jersey.connector.httpclient;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.HEAD;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,5 +27,32 @@ public class DummyRestService {
         return sleepTimeInMilliseconds;
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("post")
+    public Data post(Data data) {
+        return data;
+    }
+
+
+    static class Data {
+        private String data;
+
+        public Data() {
+        }
+
+        public Data(String data) {
+            this.data = data;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    }
 
 }
