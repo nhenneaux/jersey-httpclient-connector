f73caa9834d475c149b8adcff734539a41fb50da
Nicolas Henneaux <nicolas@henneaux.io>
2024-03-29 14:18:53 +0100

    Build with Java 22-23ea Libraries update

--- src/test/java/com/github/nhenneaux/jersey/connector/httpclient/JettyServer.java
+++ src/test/java/com/github/nhenneaux/jersey/connector/httpclient/JettyServer.java
@@ -1,13 +1,13 @@
 package com.github.nhenneaux.jersey.connector.httpclient;
 
 import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
-import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
-import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.util.ExceptionUtil;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.MultiException;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
@@ -47,7 +47,7 @@ public class JettyServer implements AutoCloseable {
         http2Connector.setPort(port);
         server.addConnector(http2Connector);
 
-        ServletContextHandler context = new ServletContextHandler("/");
+        ServletContextHandler context = new ServletContextHandler(server, "/");
         contexts.addHandler(context);
         final ResourceConfig resourceConfig = new ResourceConfig();
         for (Class<?> serviceClass : serviceClasses) {
@@ -64,7 +64,7 @@ public class JettyServer implements AutoCloseable {
             try {
                 close();
             } catch (RuntimeException closeException) {
-                ExceptionUtil.MultiException multiException = new ExceptionUtil.MultiException();
+                MultiException multiException = new MultiException();
                 multiException.add(e);
                 multiException.add(closeException);
                 multiException.ifExceptionThrowRuntime();
